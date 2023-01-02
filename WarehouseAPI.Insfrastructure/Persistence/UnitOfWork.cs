860b0d541c71aa9b911a2188536f9a7e403d53e8
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 10:35:44 -0400

    Add UoW implementation.

---
+++ WarehouseAPI.Insfrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using WarehouseAPI.ApplicationCore.Interfaces;
+using WarehouseAPI.ApplicationCore.Interfaces.Repositories;
+
+namespace WarehouseAPI.Insfrastructure.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IProductRepository products, ISaleRepository sales)
+        {
+            Products = products;
+            Sales = sales;
+        }
+
+        public IProductRepository Products { get; set; }
+        public ISaleRepository Sales { get; set; }
+    }
+}
