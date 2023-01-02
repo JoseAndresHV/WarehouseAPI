c829013ccddbceb131137ad6e1c7ba55a9ca9555
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 10:55:32 -0400

    Add repositories and uow dependency injections.

---
+++ WarehouseAPI.Insfrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WarehouseAPI.ApplicationCore.Interfaces;
+using WarehouseAPI.ApplicationCore.Interfaces.Repositories;
+using WarehouseAPI.Insfrastructure.Persistence;
+using WarehouseAPI.Insfrastructure.Persistence.Repository;
+
+namespace WarehouseAPI.Insfrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ISaleRepository, SaleRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
