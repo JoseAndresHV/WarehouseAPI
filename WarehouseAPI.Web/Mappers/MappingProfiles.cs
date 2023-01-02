48e6cdbece003f12253515aff4af56033d6fa2e7
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 12:05:35 -0400

    Add mapping profile for Product and Sale.

---
+++ WarehouseAPI.Web/Mappers/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WarehouseAPI.ApplicationCore.Entities;
+using WarehouseAPI.Web.Dtos;
+
+namespace WarehouseAPI.Web.Mappers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+
+            CreateMap<Product, CreateProductDto>();
+            CreateMap<CreateProductDto, Product>();
+
+            CreateMap<Sale, SaleDto>();
+            CreateMap<SaleDto, Sale>();
+        }
+    }
+}
