c9d8e394ec1a64631885cc0e7ca1c15a46d67a4d
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 12:15:30 -0400

    Add ApiResponse model.

---
+++ WarehouseAPI.Web/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseAPI.Web.Models
+{
+    public class ApiResponse<T> where T : class
+    {
+        public bool Success { get; set; } = false;
+        public string? Message { get; set; }
+        public T? Result { get; set; }
+    }
+}
