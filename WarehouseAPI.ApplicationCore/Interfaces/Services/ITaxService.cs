3733ac0bacf98ac1a7f052fdffedeadacb77bc38
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 09:55:45 -0400

    Add tax calculator service interface.

---
+++ WarehouseAPI.ApplicationCore/Interfaces/Services/ITaxService.cs
@@ -0,0 +1,10 @@
+﻿using WarehouseAPI.ApplicationCore.Entities;
+using WarehouseAPI.ApplicationCore.Enums;
+
+namespace WarehouseAPI.ApplicationCore.Interfaces.Services
+{
+    public interface ITaxService
+    {
+        Sale CalculateTaxAndGetSale(Product product, int qty, TaxType taxType);
+    }
+}
