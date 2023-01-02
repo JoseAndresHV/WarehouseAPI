08b4f72b77931d723578f2b254190868dd992949
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 11:29:48 -0400

    Add MissingId exception.

---
+++ WarehouseAPI.ApplicationCore/Exceptions/MissingIdException.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseAPI.ApplicationCore.Exceptions
+{
+    public class MissingIdException<T> : Exception where T : class
+    {
+        public MissingIdException() :
+            base($"The {nameof(T)} Id was not provided.")
+        {
+        }
+    }
+}
