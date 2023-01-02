5388c88ed48c3fed6da9691120132ddea2c41dbc
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 10:00:39 -0400

    Add Product and Sale SQL commands.

---
+++ WarehouseAPI.Insfrastructure/Sql/Commands/ProductCommands.cs
@@ -0,0 +1,30 @@
+﻿namespace WarehouseAPI.Insfrastructure.Sql.Commands
+{
+    public class ProductCommands
+    {
+        public static string Select =>
+            @"SELECT * FROM Product";
+
+        public static string SelectWhereId =>
+            @"SELECT * FROM Product
+              WHERE Id = @Id";
+
+        public static string Insert =>
+            @"INSERT INTO 
+              Product(ProductName, Description, Stock, Price)
+              OUTPUT INSERTED.Id
+              VALUES(@ProductName, @Description, @Stock, @Price)";
+
+        public static string Update =>
+            @"UPDATE Product SET
+                ProductName = @ProductName,
+                Description = @Description,
+                Stock = @Stock,
+                Price = @Price
+              WHERE Id = @Id";
+
+        public static string Delete =>
+            @"DELETE FROM Product
+              WHERE Id = @Id";
+    }
+}
---
+++ WarehouseAPI.Insfrastructure/Sql/Commands/SaleCommands.cs
@@ -0,0 +1,32 @@
+﻿namespace WarehouseAPI.Insfrastructure.Sql.Commands
+{
+    public class SaleCommands
+    {
+        public static string Select =>
+            @"SELECT * FROM Sale";
+
+        public static string SelectWhereId =>
+            @"SELECT * FROM Sale
+              WHERE Id = @Id";
+
+        public static string Insert =>
+            @"INSERT INTO
+              Sale(ProductId, Qty, Subtotal, Iva, Total, DateTime)
+              OUTPUT INSERTED.Id
+              VALUES(@ProductId, @Qty, @Subtotal, @Iva, @Total, @DateTime)";
+
+        public static string Update =>
+            @"UPDATE Sale SET
+                ProductId = @ProductId, 
+                Qty = @Qty, 
+                Subtotal = @Subtotal,
+                Iva = @Iva, 
+                Total = @Total,
+                DateTime = @DateTime
+              WHERE Id = @Id";
+
+        public static string Delete =>
+            @"DELETE FROM Sale
+              WHERE Id = @Id";
+    }
+}
