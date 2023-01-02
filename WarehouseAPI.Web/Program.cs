d70e79c5d4a6dcefb9fa105acc3d9bdf5d2db7c9
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 12:07:28 -0400

    Add AutoMapper and Infrastructure services injection.

--- WarehouseAPI.Web/Program.cs
+++ WarehouseAPI.Web/Program.cs
@@ -1,6 +1,12 @@
+using WarehouseAPI.Insfrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddServices();
+
+//Automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
