5b8900af75083fe36ba17658e30672af8ce67d3a
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 09:34:10 -0400

    Add Product and Sale entities.

---
+++ WarehouseAPI.ApplicationCore/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseAPI.ApplicationCore.Entities
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string? Description { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ WarehouseAPI.ApplicationCore/Entities/Sale.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseAPI.ApplicationCore.Entities
+{
+    public class Sale
+    {
+        public int? Id { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Iva { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
