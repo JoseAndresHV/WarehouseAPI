3f004a9a0c5fbdb5489b71fe9f3c39145f66f52d
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-01-02 09:50:48 -0400

    Add TaxType enum.

---
+++ WarehouseAPI.ApplicationCore/Enums/TaxTypes.cs
@@ -0,0 +1,19 @@
+﻿namespace WarehouseAPI.ApplicationCore.Enums
+{
+    public enum TaxType
+    {
+        None,
+        IVA,
+        Other
+    }
+
+    public static class TaxTypes
+    {
+        public static readonly Dictionary<TaxType, decimal> Values = new()
+        {
+            [TaxType.None] = .0m,
+            [TaxType.IVA] = .13m,
+            [TaxType.Other] = .05m,
+        };
+    }
+}
